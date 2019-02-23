63c3ebe72bf5b3b45cdfdf9964323e2e8670e68f
chickey96 <c.m.hickey06@gmail.com>
2019-02-22 16:37:10 -0800

    W7D5

---
+++ benchBnB/frontend/actions/session_actions.js
@@ -0,0 +1,28 @@
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_CURRENT_ERRORS = 'RECEIVE_CURRENT_ERRORS';
+import { signup, login, logout } from '../util/session_api_util';
+
+export const receiveCurrentUser = user => ({
+    type: RECEIVE_CURRENT_USER,
+    user,
+});
+
+export const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER,
+});
+
+export const receiveErrors = errors => ({
+    type: RECEIVE_CURRENT_ERRORS,
+    errors
+});
+
+export const login = user => dispatch => login(user)
+  .then(user => dispatch(receiveCurrentUser(user)));
+
+export const logout = () => dispatch => logout()
+  .then( () => dispatch(logoutCurrentUser()));
+
+export const signup = user => dispatch => signup(user)
+  .then(user => dispatch(receiveCurrentUser(user)));
---
+++ benchBnB/frontend/reducers/users_reducer.js
@@ -0,0 +1,8 @@
+import {
+    RECEIVE_CURRENT_USER,
+    receiveCurrentUser
+} from '../actions/session_actions'
+
+export const usersReducer = (state=_nullState, action) => ({
+
+})
